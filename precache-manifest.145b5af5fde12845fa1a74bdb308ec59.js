f225559313fe33222d18b9b989e7c5181c10b247
Daniel <dann.santlar@gmail.com>
2019-07-11 13:41:51 -0700

    Updates

---
+++ precache-manifest.145b5af5fde12845fa1a74bdb308ec59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa1baf94d585396277d0eabc38e8de4",
+    "url": "/react-course-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c0b9028538c6edb750fc",
+    "url": "/react-course-monsters-rolodex/static/css/main.0595d80d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-course-monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c0b9028538c6edb750fc",
+    "url": "/react-course-monsters-rolodex/static/js/main.4a74c105.chunk.js"
+  },
+  {
+    "revision": "e3eb4be19bc8b669eabf",
+    "url": "/react-course-monsters-rolodex/static/js/runtime~main.7dcbc653.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-course-monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
